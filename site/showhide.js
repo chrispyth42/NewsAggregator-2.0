5e9f6332f02ea3898c88a89458084417958ea853
Chris <49737464+chrispyth42@users.noreply.github.com>
2020-02-24 15:52:51 -0700

    Add files via upload

---
+++ site/showhide.js
@@ -0,0 +1,12 @@
+// http://www.jtricks.com/javascript/blocks/showinghiding.html
+function togglePageElementVisibility(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    if (obj.style.display == 'none')  
+        obj.style.display = 'block';  
+    else  
+        obj.style.display = 'none';  
+    return false;  
+}  
